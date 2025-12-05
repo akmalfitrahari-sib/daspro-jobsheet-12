07bd0125426877f44cbe1c1e0de00ca6e7a595a3
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2025-12-05 19:26:47 +0700

    Rekap cafe v2

--- RekapPenjualanCafe03.java
+++ RekapPenjualanCafe03.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 public class RekapPenjualanCafe03 {
-    static String[] menu = {"Kopi", "Teh", "Es Kelapa Muda", "Roti Bakar", "Gorengan"};
+    static String[] menu;
+    static int[][] dataPenjualan;
+    static int jmlMenu, jmlHari;
     
-    public static void menu03(String[] menu) {   
+    public static void menu03() {   
     System.out.println("=== Daftar menu === ");
         for (String item : menu) {
             System.out.println("- " + item);
@@ -9,59 +12,81 @@ public class RekapPenjualanCafe03 {
 
     }
 
-    static int[][] dataPenjualan = {
-        {20, 20, 25, 20, 10, 60, 10},
-        {30, 80, 40, 10, 15, 20, 25},
-        {5, 9, 20, 25, 10, 30, 6},
-        {50, 8, 17, 18, 10, 30, 6},
-        {15, 10, 16, 15, 10, 10, 55}
-    };
-
     public static void tabel() {
         System.out.printf("%-15s", "Menu");
-        for (int i = 1; i <= 7; i++) {
+        for (int i = 1; i <= jmlHari; i++) {
             System.out.printf("Hari %d\t", i);
         }
         System.out.println();
 
-        for (int j = 0; j < menu.length; j++) {
+        for (int j = 0; j < jmlMenu; j++) {
             System.out.printf("%-15s", menu[j]);
-            for (int k = 0; k < 7; k++) {
+            for (int k = 0; k < jmlHari; k++) {
                 System.out.printf("%d\t", dataPenjualan[j][k]);
             }
             System.out.println();
         }
     }
 
-    public static void menuTertinggi(int max, int index) {
-        for (int i = 0; i < menu.length; i++) {
+    public static void menuTertinggi() {
+        int max = -1;
+        int index = 0;
+        for (int i = 0; i < jmlMenu; i++) {
             int total = 0;
-            for (int j = 0; j < 7; j++) total += dataPenjualan[i][j];
+            for (int j = 0; j < jmlHari; j++) total += dataPenjualan[i][j];
             if (total > max) {
                 max = total;
                 index = i;
             }
         }
-        System.out.println();
-        System.out.print("=== Menu dengan total penjualan tertinggi ===\n");
+       
+        System.out.println("\n=== Menu dengan total penjualan tertinggi ===");
         System.out.println(menu[index] + " = " + max);
     }
 
     public static void rata2() {
-        System.out.println();
-        System.out.println("=== Rata-rata penjualan setiap menu ===");
-        for (int i = 0; i < menu.length; i++) {
+        System.out.println("\n=== Rata-rata penjualan setiap menu ===");
+        for (int i = 0; i < jmlMenu; i++) {
             int total = 0;
-            for (int j = 0; j < 7; j++) total += dataPenjualan[i][j];
-            double avg = total / 7.0;
+            for (int j = 0; j < jmlHari; j++) total += dataPenjualan[i][j];
+            double avg = total / (double) jmlHari;
             System.out.printf("%-15s: %.2f\n", menu[i], avg);
         }
     }
     public static void main(String[] args) {
-        menu03(menu);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah menu : ");
+        jmlMenu = sc.nextInt();
+        sc.nextLine();
+
+        menu = new String[jmlMenu];
+        for (int i = 0; i < jmlMenu; i++) {
+            System.out.print("Nama menu ke-" + (i+1) + ": ");
+            menu[i] = sc.nextLine();
+        }
+
+        System.out.print("\nMasukkan jumlah hari penjualan: ");
+        jmlHari = sc.nextInt();
+
+        dataPenjualan = new int[jmlMenu][jmlHari];
+
+        System.out.println("\n=== Input Data Penjualan ===");
+        for (int i = 0; i < jmlMenu; i++) {
+            for (int j = 0; j < jmlHari; j++) {
+                System.out.print(menu[i] + " hari ke-" + (j+1) + ": ");
+                dataPenjualan[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        menu03();
         System.out.println();
         tabel();
-        menuTertinggi(-1, 0);
+        menuTertinggi();
         rata2();
+
+        sc.close();
     }
 }
